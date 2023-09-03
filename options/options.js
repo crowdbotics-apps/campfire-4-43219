da47d86f50f555860d8d53c540d0decc8708ab40
Crowdbotics <team@crowdbotics.com>
2023-09-03 20:17:49 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "campfire-4-43219",
+  url: "https://campfire-4-43219.botics.co",
+  api: "https://campfire-4-43219.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
